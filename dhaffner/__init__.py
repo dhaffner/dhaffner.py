5ad17f8577f18346d5cffd1b284905e531947cc2
Dustin Haffner <dh@xix.org>
2013-04-01 10:31:54 -0700

    add compose to main module

--- dhaffner/__init__.py
+++ dhaffner/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-__all__ = ('builtins', 'functions', 'iterators', 'misc')
+__all__ = ('builtins', 'functions', 'iterators', 'misc', 'compose')
 
 import builtins
 import functions
 import misc
 import iterators
+
+from common import compose
