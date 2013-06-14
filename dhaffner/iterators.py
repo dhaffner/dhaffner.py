b386823a0fc0e57d15ca5d924cb630578115547e
Dustin Haffner <dh@xix.org>
2013-06-14 09:48:09 -0700

    clean up iterators

--- dhaffner/iterators.py
+++ dhaffner/iterators.py
@@ -8,7 +8,7 @@ __all__ = ('compact', 'consume', 'drop', 'exhaust', 'first', 'flatten',
 
 from collections import deque, Sequence
 from functools import partial
-from itertools import islice, chain, tee, repeat
+from itertools import islice, chain, tee
 from operator import mul
 
 from common import compose, filter, filterfalse, zip
@@ -19,7 +19,8 @@ compact = partial(filter, bool)
 
 
 def consume(iterator, n=None, next=next, islice=islice, deque=deque):
-    """Consume a given amount of elements in from a generator. If no amount is
+    """
+    Consume a given amount of elements in from a generator. If no amount is
     specified, exhaust the entire sequence.
     """
     if n is not None:
@@ -29,17 +30,23 @@ def consume(iterator, n=None, next=next, islice=islice, deque=deque):
 
 
 def dotproduct(vec1, vec2, sum=sum, map=map, mul=mul):
-    """Compute and return the dot product of two vectors (sequences)."""
+    """
+    Compute and return the dot product of two vectors (sequences).
+    """
     return sum(map(mul, vec1, vec2))
 
 
 def drop(iterable, n, islice=islice):
-    """Drop the first n elements of the given iterable."""
+    """
+    Drop the first n elements of the given iterable.
+    """
     return islice(iterable, n, None)
 
 
 def exhaust(iterator, deque=deque):
-    """Exhaust a given iterator."""
+    """
+    Exhaust a given iterator.
+    """
     deque(iterator, maxlen=0)
 
 
@@ -52,10 +59,10 @@ flatten = chain.from_iterable
 
 
 def issequence(obj, isinstance=isinstance, Sequence=Sequence):
-    """Determine whether obj is a sequence. Strings are not considered
+    """
+    Determine whether obj is a sequence. Strings are not considered
     sequences.
     """
-
     try:
         obj.strip
     except:
@@ -71,16 +78,32 @@ last = partial(reduce, lambda _, y: y)
 
 
 def nth(iterable, n, next=next, islice=islice, default=None):
-    """Returns the nth item or a default value
+    """
+    Return the nth item or a default value from an iterable.
 
     http://docs.python.org/3.4/library/itertools.html#itertools-recipes
     """
     return next(islice(iterable, n, None), default)
 
 
+def partition(items, predicate=bool):
+    """
+    Partition a given sequence into two  subsequences: those for which
+    predicate returns True and those for which it returns False.
+
+    Source: http://nedbatchelder.com/blog/201306/filter_a_list_into_two_parts.html
+    """
+
+    a, b = tee((predicate(item), item) for item in items)
+    return ((item for pred, item in a if not pred),
+            (item for pred, item in b if pred))
+
+
 def pick(iterable):
-    """Yield elements of sequence, repeating the last element infinitely after
-    the sequence is iterated over."""
+    """
+    Yield elements of sequence, repeating the last element infinitely after
+    the sequence is iterated over.
+    """
     for element in iterable:
         yield element
     while True:
@@ -95,7 +118,8 @@ class reusable(object):
         return self
 
     def reset(self):
-        """Reset the iterator to the start. Discard any remaining values in the
+        """
+        Reset the iterator to the start. Discard any remaining values in the
         current iteration.
         """
         self.iterator, self.iterable = tee(self.iterable)
@@ -113,18 +137,17 @@ class reusable(object):
 split = compose(lambda iterator, next=next: (next(iterator), iterator), iter)
 
 
-def tag(t, iterable, zip=zip, repeat=repeat):
-    """Tag (pair) t with each element in the given sequence"""
-    return zip(repeat(t), iterable)
-
-
 def take(n, iterable, islice=islice):
-    """Take the first n elements of the given iterable."""
+    """
+    Take the first n elements of the given iterable.
+    """
     return islice(iterable, n)
 
 
 def unique(iterable, filterfalse=filterfalse):
-    """Return only unique elements from the sequence."""
+    """
+    Return only unique elements from the sequence.
+    """
     seen = set()
     add = seen.add
     for element in filterfalse(seen.__contains__, iterable):
