4e9f9ef9d2419fdb4a88cf43f72aaa74689f9052
Dustin Haffner <dh@xix.org>
2014-01-11 10:27:52 -0800

    add test_builtins

---
+++ tests/test_builtins.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from dhaffner import builtins
+
+import random
+import unittest
+import time
+import operator
+
+
+class TestCommon(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
